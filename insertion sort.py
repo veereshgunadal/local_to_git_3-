5a779bddfcc4d688a5a1cfeed9382b8a2a4232a0
veereshgunadal <gunadalveeresh@gmail.com>
2024-07-23 06:42:39 -0700

    added if condition

--- insertion sort.py
+++ insertion sort.py
@@ -1,4 +1,11 @@
 def insertion(l):
     for i in range(len(l)-1):
-        temp = i
+        small_index = i
         for j in range(i+1, len(l)):
+            if l[j] < l[j-1]:
+                small_index = j
+        temp = l[i]
+        l[i] = l[small_index]
+        l[j] = temp
+
+        
