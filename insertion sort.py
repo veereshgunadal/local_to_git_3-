60b67660b15df47e9a5c89986f00a99475ae2769
veereshgunadal <gunadalveeresh@gmail.com>
2024-07-28 01:26:04 -0700

    insertionsort

--- insertion sort.py
+++ insertion sort.py
@@ -6,8 +6,22 @@ def insertion(l):
                 small_index = j
         temp = l[i]
         l[i] = l[small_index]
-        l[j] = temp
+        l[small_index] = temp
+                
     return l
 
-l = [1,3,2,4,9]
+l = [1,3,2,4,9,7,8]
 print(insertion(l))
+print('1')
+
+
+def selection(l):
+    for i in range(len(l)-1):
+        for j in range(i+1, 0 , -1):
+            if l[j] < l[j-1]:
+                temp1 = l[j]
+                l[j] = l[j-1]
+                l[j-1] = temp1
+    return l
+l = [1,3,2,4,9]
+print(selection(l))
